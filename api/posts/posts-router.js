23d4e7cff6da757ba288cbf97db2b993d1ba16a0
Kohlten Bills <kohltenbillsy@gmail.com>
2022-07-13 19:21:18 -0600

    15 passed

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -6,14 +6,14 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
     Post.find()
-    .then(found => {
-        res.json(found)
-    })
-    .catch(err => {
-        res.status(500).json({
-            message: "The posts information could not be retrieved"
+        .then(found => {
+            res.json(found)
         })
-    })
+        .catch(err => {
+            res.status(500).json({
+                message: "The posts information could not be retrieved"
+            })
+        })
 })
 
 router.get('/:id', async (req, res) => {
@@ -41,18 +41,18 @@ router.post('/', (req, res) => {
         })
     } else {
         Post.insert({ title, contents })
-        .then(({ id }) =>{
-            return Post.findById(id)
-        })
-        .then(post => {
-            res.status(201).json(post)
-        })
-        .catch(err => {
-            res.status(500).json({
-                message: "There was an error while saving the post to the database"
+            .then(({ id }) => {
+                return Post.findById(id)
             })
-        })
-    } 
+            .then(post => {
+                res.status(201).json(post)
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "There was an error while saving the post to the database"
+                })
+            })
+    }
 })
 
 router.delete('/:id', async (req, res) => {
@@ -74,7 +74,36 @@ router.delete('/:id', async (req, res) => {
 })
 
 router.put('/:id', (req, res) => {
-
+    const { title, contents } = req.body
+    if (!title || !contents) {
+        res.status(400).json({
+            message: "Please provide title and contents for the post"
+        })
+    } else {
+        Post.findById(req.params.id)
+            .then(stuff => {
+                if (!stuff) {
+                    res.status(404).json({
+                        message: 'The post with the specified ID does not exist'
+                    })
+                } else {
+                    return Post.update(req.params.id, req.body)
+                }
+            })
+            .then(data => {
+                if (data) {
+                    return Post.findById(req.params.id)
+                }
+            })
+            .then(post => {
+                res.json(post)
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: 'The posts information cound not be retrieved'
+                })
+            })
+    }
 })
 
 router.get('/:id/messages', (req, res) => {
